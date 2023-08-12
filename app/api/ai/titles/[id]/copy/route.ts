a7c103635e4637b6b05199a47b04f15144ab6a56
Jason Baddley <jasonbaddley@gmail.com>
2023-08-12 13:46:57 -0600

    deploying

--- app/api/ai/titles/[id]/copy/route.ts
+++ app/api/ai/titles/[id]/copy/route.ts
@@ -1,14 +1,7 @@
 import { NextResponse } from "next/server";
 import { getAdCopy } from "../../../../../lib/writeAds";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "../../../../auth/[...nextauth]/route";
 
 export async function POST(req: Request) {
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    return NextResponse.json({ message: "You are not logged in.", status: "error" });
-  }
   const { context, title } = await req.json();
   const data = await getAdCopy(context, title);
   console.log({ data });
