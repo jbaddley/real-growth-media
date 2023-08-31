22722b57e60c46ef97ef84b16c6a571c3e1a6b07
Jason Baddley <jasonbaddley@gmail.com>
2023-08-31 17:34:39 -0600

    deploying

--- app/components/FacebookPixel.tsx
+++ app/components/FacebookPixel.tsx
@@ -10,8 +10,8 @@ export const FacebookPixelEvents: React.FC = () => {
     import("react-facebook-pixel")
       .then((x) => x.default)
       .then((ReactPixel) => {
-        console.log({ FACEBOOKPIXEL_ID: process.env.NEXT_PUBLIC_FACEBOOKPIXEL_ID });
-        ReactPixel.init(process.env.NEXT_PUBLIC_FACEBOOKPIXEL_ID); //don't forget to change this
+        console.log({ FACEBOOKPIXEL_ID: process.env.NEXT_PUBLIC_FACEBOOKPIXEL_ID || "1447250482703220" });
+        ReactPixel.init(process.env.NEXT_PUBLIC_FACEBOOKPIXEL_ID || "1447250482703220"); //don't forget to change this
         ReactPixel.pageView();
       });
   }, [pathname, searchParams]);
