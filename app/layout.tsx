5b0b376629dce4d12d5b0d037921540a37347251
Jason Baddley <jasonbaddley@gmail.com>
2023-08-31 17:18:04 -0600

    deploying

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,26 +3,9 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { useEffect } from "react";
-import { Router } from "next/router";
 import FacebookPixel from "./components/FacebookPixel";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Pro Peak AI",
-  description: "Pro Peak AI - A simple tool for creating contextual marketing materials",
-  icons: {
-    icon: {
-      url: "/favicon.ico",
-      type: "image/icon",
-    },
-  },
-  shortcut: {
-    url: "/favicon.svg",
-    type: "image/svg",
-  },
-};
 
 export default function RootLayout({ children }) {
   return (
