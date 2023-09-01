633f29817a994b79ec64066552c13656cf7850df
Jason Baddley <jasonbaddley@gmail.com>
2023-09-01 11:01:09 -0600

    deploying

--- app/(public)/roas-calc/page.tsx
+++ app/(public)/roas-calc/page.tsx
@@ -1,10 +1,13 @@
 "use client";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import RoasCalc, { StorageInput, defaultInput } from "../../components/RoasCalc";
-import { Button, Modal, Tabs } from "flowbite-react";
+import { Button, Modal, Tabs, TextInput } from "flowbite-react";
 import useSWR from "swr";
 import { Fetcher } from "../../lib/fetcher";
 import { Contact } from "@prisma/client";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import e from "cors";
 const videoSrc = "https://storage.googleapis.com/msgsndr/nrw8M8zQccEYIjAMiR22/media/64f0ef8a1181e8f643212216.mp4";
 const defaultTabs: Record<string, StorageInput> = {
   defaultTab: {
@@ -22,26 +25,51 @@ const getInitial = (): Record<string, StorageInput> => {
 };
 
 export default function () {
+  const submitButton = useRef<HTMLButtonElement>(null);
   const [open, setOpen] = useState<boolean>(false);
+  const [openContact, setOpenContact] = useState<boolean>(false);
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const email = useMemo(() => {
     const search = new URLSearchParams(globalThis.window.location.search);
-    return search.get("email");
+    return search.get("email") || globalThis.localStorage.getItem("pp-contact-email");
   }, [globalThis.window.location.search]);
 
-  const { data: contact, isLoading } = useSWR(["contact", email], async () => {
+  const {
+    data: contact,
+    isLoading,
+    mutate,
+  } = useSWR(["contact", email], async () => {
     if (!email) {
       return undefined;
     }
     const { data } = await Fetcher.get<Contact>(`/api/webhooks/contacts?email=${email}`);
+    globalThis.localStorage.setItem("pp-contact-email", data.email);
     return data;
   });
 
   useEffect(() => {
-    if (!isLoading && contact?.watchedRoas !== "100%") {
-      setOpen(true);
+    if (!isLoading && contact) {
+      setValue("email", contact.email);
+      setValue("firstName", contact.firstName);
+      setValue("lastName", contact.lastName);
+      setValue("phone", contact.phone);
+      setValue("allowContact", contact.allowContact);
+      globalThis.localStorage.setItem("pp-contact-email", contact.email);
     }
   }, [contact, isLoading]);
+
+  useEffect(() => {
+    if (!email) {
+      setOpenContact(true);
+    }
+  }, [email]);
+
   const [tabs, setTabs] = useState<Record<string, StorageInput>>(getInitial());
   const [activeTab, setActiveTab] = useState<string>();
 
@@ -99,8 +127,17 @@ export default function () {
     setOpen(false);
   };
 
+  const handleSaveContact = async (formValues: Contact) => {
+    await Fetcher.post<Contact, Contact>("/api/webhooks/contacts", formValues);
+    mutate();
+    setOpenContact(false);
+  };
+
   return (
     <div className='p-2'>
+      <div className='flex flex-row-reverse'>
+        <Button onClick={() => setOpen(true)}>Show Demo</Button>
+      </div>
       <Tabs.Group tabIndex={activeTabIndex}>
         {Object.entries(tabs).map(([key, value]) => (
           <Tabs.Item key={key} active={key === activeTab} title={value.displayName}>
@@ -121,6 +158,67 @@ export default function () {
       <Modal show={open} onClose={() => setOpen(false)} size={"5xl"}>
         <Modal.Header>Quick Demo!</Modal.Header>
         <Modal.Body>{open && <video src={videoSrc} controls autoFocus onEnded={handleCaptureEnded} />}</Modal.Body>
+        <Modal.Footer className='flex'>
+          <div className='flex-grow'>
+            <Link className='mx-4' target='blank' href='https://book.realgrowth.media/booking'>
+              Book a Call
+            </Link>
+          </div>
+          <div>
+            <Button onClick={() => setOpen(false)}>Close</Button>
+          </div>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={openContact} onClose={() => setOpenContact(false)}>
+        <Modal.Header>In Order to Use Our ROAS Calculator, Please Confirm Your Contact Information</Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleSubmit(handleSaveContact)} className='space-y-4'>
+            <p>We will never sell your information.</p>
+            <p>We use the information to improve our free tools and create new ones.</p>
+            <div>
+              <label>First Name</label>
+              <TextInput {...register("firstName")} />
+            </div>
+            <div>
+              <label>Last Name</label>
+              <TextInput {...register("lastName")} />
+            </div>
+            <div>
+              <label>Email Address</label>
+              <TextInput {...register("email")} />
+            </div>
+            <div>
+              <label>Phone Number</label>
+              <TextInput {...register("phone")} />
+            </div>
+            <p>May we reach out to you about additional tools and services?</p>
+            <label>
+              <input type='checkbox' {...register("allowContact")} /> Yes, you can contact me.
+            </label>
+            <button ref={submitButton} className='hidden' type='submit'>
+              OK
+            </button>
+          </form>
+        </Modal.Body>
+        <Modal.Footer className='flex'>
+          <div>
+            <Link className='mx-4' target='blank' href='https://book.realgrowth.media/booking'>
+              Book a Call
+            </Link>
+          </div>
+          <div className='flex flex-grow flex-row-reverse'>
+            <Button
+              onClick={() => {
+                submitButton.current.click();
+              }}
+            >
+              Submit
+            </Button>
+            <Button className='mx-4' onClick={() => setOpenContact(false)}>
+              Close
+            </Button>
+          </div>
+        </Modal.Footer>
       </Modal>
     </div>
   );
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,6 +4,8 @@ import logo from "../assets/rgm-logo.png";
 import Image from "next/image";
 import { Navbar } from "flowbite-react";
 import { useAuth } from "@clerk/nextjs";
+import Link from "next/link";
+import Button from "./Button";
 
 const Header = () => {
   const { isLoaded, isSignedIn } = useAuth();
@@ -13,26 +15,11 @@ const Header = () => {
         <Image src={logo} alt='RGM Logo' height={48} />
       </Navbar.Brand>
       <div className='flex md:order-2'>
-        <SignInButton />
+        <Link className='text-white' target='blank' href='https://freetraining.realgrowth.media/free-calc-booking'>
+          <Button>Book a Call</Button>
+        </Link>
       </div>
-      <Navbar.Collapse>
-        <Navbar.Link active href='/'>
-          <p>Home</p>
-        </Navbar.Link>
-        <Navbar.Link active href='/roas-calc'>
-          <p>ROAS Calc</p>
-        </Navbar.Link>
-        <Navbar.Link active href='https://realgrowth.media'>
-          <p>Learn More</p>
-        </Navbar.Link>
-        {isLoaded && isSignedIn ? (
-          <>
-            <Navbar.Link active href='/pro-peak-ai'>
-              <p>Pro Peak AI</p>
-            </Navbar.Link>
-          </>
-        ) : null}
-      </Navbar.Collapse>
+      <Navbar.Collapse></Navbar.Collapse>
     </Navbar>
   );
 };
