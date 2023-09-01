dd373875ee6909b0d569232165c24a228dc1018e
Jason Baddley <jasonbaddley@gmail.com>
2023-08-31 18:10:47 -0600

    deploying

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,22 @@
-import { authMiddleware } from "@clerk/nextjs";
-export default authMiddleware({
-  // "/" will be accessible to all users
-  publicRoutes: ["/", "/roas-calc"],
-});
+import { NextResponse } from "next/server";
 
+export function middleware() {
+  // retrieve the current response
+  const res = NextResponse.next();
+
+  // add the CORS headers to the response
+  res.headers.append("Access-Control-Allow-Credentials", "true");
+  res.headers.append("Access-Control-Allow-Origin", "*"); // replace this your actual origin
+  res.headers.append("Access-Control-Allow-Methods", "GET,DELETE,PATCH,POST,PUT");
+  res.headers.append(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  return res;
+}
+
+// specify the path regex to apply the middleware to
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: "/api/*",
 };
