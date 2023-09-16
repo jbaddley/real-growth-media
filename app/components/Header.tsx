a73b0ab4671710e7d1cda1936c7b120a64ce53e6
Jason Baddley <jasonbaddley@gmail.com>
2023-09-16 09:20:03 -0600

    deploying

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,15 +5,29 @@ import { Navbar } from "flowbite-react";
 import { useAuth } from "@clerk/nextjs";
 import Link from "next/link";
 import Button from "./Button";
+import { FaPhone } from "react-icons/fa";
 
 const Header = () => {
   const { isLoaded, isSignedIn } = useAuth();
+  const handleCopy = () => {
+    navigator.clipboard.writeText("8015084303").then(
+      () => {
+        console.log(`Copied ${"8015084303"}`);
+      },
+      () => {
+        console.error(`Failed to copy ${"8015084303"}`);
+      }
+    );
+  };
   return (
     <Navbar fluid className='bg-black py-6'>
       <Navbar.Brand href='https://homeservices.realgrowth.media/book-a-free-consultation'>
         <Image src={logo} alt='RGM Logo' height={48} />
       </Navbar.Brand>
       <div className='flex md:order-2'>
+        <a onClick={handleCopy} className='mx-4 my-2 text-white' href='tel:8015084303'>
+          801-508-4303
+        </a>
         <Link
           className='text-white'
           target='blank'
